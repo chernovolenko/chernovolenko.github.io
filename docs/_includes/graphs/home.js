434c53ce718163c5a1488e283a8e958dda3f6371
chernovolenko <pavlo.chernovolenko@gmail.com>
2021-09-12 23:04:14 +0200

    js name fix

--- docs/_includes/graphs/home.js
+++ docs/_includes/graphs/home.js
@@ -1,19 +1,14 @@
-d3.xml("assets/posts/sweden/map.svg")
-  .then(data => {
-    d3.select("#map").node().append(data.documentElement)
-  });
+var svg = d3.select("#map")
+    .append("svg")
+    .attr("style", "solid 1px black")
+    .attr("width", "100%");
 
-// var svg = d3.select("#map")
-//     .append("svg")
-//     .attr("style", "solid 1px black")
-//     .attr("width", "100%");
-//
-// var g = svg.append("g");
-// d3.json("/assets/posts/sweden/map.svg", function (error, json) {
-//     g.selectAll("path")
-//            .data(json.features)
-//            .enter()
-//            .append("path")
-//            .attr("d", path)
-//            .style("fill", "red");
-// });
+var g = svg.append("g");
+d3.json("assets/posts/sweden/map.svg", function (error, json) {
+    g.selectAll("path")
+           .data(json.features)
+           .enter()
+           .append("path")
+           .attr("d", path)
+           .style("fill", "red");
+});
