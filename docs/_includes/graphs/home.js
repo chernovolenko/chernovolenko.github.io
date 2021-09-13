d7d7808b07f4f7caaa82f1ec69d89a41df7ed441
chernovolenko <pavlo.chernovolenko@gmail.com>
2021-09-13 20:24:23 +0200

    Update home.js

--- docs/_includes/graphs/home.js
+++ docs/_includes/graphs/home.js
@@ -4,11 +4,15 @@ var svg = d3
   .attr("width", "100%");
 var group = svg.append("g");
 
-d3.json("/assets/posts/sweden/sweden-counties.geojson", function (error, json) {
-    g.selectAll("path")
-           .data(json.features)
-           .enter()
-           .append("path")
-           .attr("d", path)
-           .style("fill", "red");
-});
+d3.json("/assets/posts/sweden/sweden-counties.geojson", function(error, geoData) {
+    group.append("path").attr("d", path(geoData));
+}
+
+// d3.json("/assets/posts/sweden/sweden-counties.geojson", function (error, json) {
+//     g.selectAll("path")
+//            .data(json.features)
+//            .enter()
+//            .append("path")
+//            .attr("d", path)
+//            .style("fill", "red");
+// });
